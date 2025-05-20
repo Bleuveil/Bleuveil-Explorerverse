b9518f53e6a47b4fa6959da01090c5cdbbe771b5
Bleuveil <care@klahs.com>
2025-05-20 11:11:20 -0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "bleuveil-cache-v1";
+const CACHE_FILES = [
+  "./index.html",
+  "./main.css",
+  "./main.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(CACHE_FILES);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
